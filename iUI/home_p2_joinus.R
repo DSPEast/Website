e9d6336da220af4fed2ec1589664ea83c3ccd44e
Ke Min <kemin.business@gmail.com>
2018-06-09 12:11:56 -0300

    Adjusted input width

--- iUI/home_p2_joinus.R
+++ iUI/home_p2_joinus.R
@@ -8,8 +8,9 @@ div_home_p2_joinus <- tags$div(
     id = "panel2_content",
     tags$h5(style = "color:rgba(96, 96, 94, 1)", "PLEASE JOIN US"),
     br(),
-    tags$div(style = "margin:0; ", textInput("req_email", NULL, "your_email@email.com")),
+    tags$div(style = "margin:0; ", textInput("req_email", NULL, "your.email@email.com", width = "150px")),
     tags$div(style = "margin:0; ", actionButton("req_join", "Request now"))
   ),
   tags$div(class = "panel2_space")
 )
+
